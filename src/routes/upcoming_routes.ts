b376cd375cc0c2bb593654ff0d4d9ee360b84a62
henry-n-projects <henryn.contact@gmail.com>
2026-01-02 01:15:56 +1100

    update: endpoints

--- src/routes/upcoming_routes.ts
+++ src/routes/upcoming_routes.ts
@@ -131,111 +131,114 @@ router.get("/past", async (req: Request, res: Response, next: NextFunction) => {
 });
 
 //POST create a new upcoming session (status = SCHEDULED)
-router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-  // Extract user from request
-  const { user } = req as AuthRequest;
+router.post(
+  "/session/add",
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Extract user from request
+    const { user } = req as AuthRequest;
 
-  //Validate that user is logged in
-  if (!user) {
-    return next(new AppError(401, "Not authenticated", true));
-  }
-
-  try {
-    // Type assertion for payload from client
-    const body = req.body as CreateUpcomingBody;
-
-    // Validate required fields exist
-    if (!body.start_at) {
-      return res.status(400).json({
-        status: "error",
-        message: "start_at is missing",
-      });
-    }
-    const now = new Date();
-    //Convert start_at and end_at to date type
-    const startAt = new Date(body.start_at);
-    const endAt = body.end_at ? new Date(now) : null;
-
-    if (startAt < now) {
-      return res.status(400).json({
-        status: "error",
-        message: "start_at must be a future date",
-      });
+    //Validate that user is logged in
+    if (!user) {
+      return next(new AppError(401, "Not authenticated", true));
     }
 
-    // Validate date is in correct format
-    if (isNaN(startAt.getTime()) || (endAt && isNaN(endAt.getTime()))) {
-      return res.status(400).json({
-        status: "error",
-        message: "start_at and end_at is in invalid format",
-      });
-    }
+    try {
+      // Type assertion for payload from client
+      const body = req.body as CreateUpcomingBody;
 
-    //Handle tag creation or selection
-    let tagId = body.tag_id;
+      // Validate required fields exist
+      if (!body.start_at) {
+        return res.status(400).json({
+          status: "error",
+          message: "start_at is missing",
+        });
+      }
+      const now = new Date();
+      //Convert start_at and end_at to date type
+      const startAt = new Date(body.start_at);
+      const endAt = body.end_at ? new Date(now) : null;
 
-    // If user creates a new tag instead of choosing existing tag
-    if (!tagId && body.new_tag_name && body.new_tag_color) {
-      const newTag = await prisma.tag.create({
+      if (startAt < now) {
+        return res.status(400).json({
+          status: "error",
+          message: "start_at must be a future date",
+        });
+      }
+
+      // Validate date is in correct format
+      if (isNaN(startAt.getTime()) || (endAt && isNaN(endAt.getTime()))) {
+        return res.status(400).json({
+          status: "error",
+          message: "start_at and end_at is in invalid format",
+        });
+      }
+
+      //Handle tag creation or selection
+      let tagId = body.tag_id;
+
+      // If user creates a new tag instead of choosing existing tag
+      if (!tagId && body.new_tag_name && body.new_tag_color) {
+        const newTag = await prisma.tag.create({
+          data: {
+            user_id: user.id,
+            name: body.new_tag_name,
+            color: body.new_tag_color,
+            created_at: new Date(),
+          },
+        });
+
+        tagId = newTag.id;
+      }
+
+      // If still no tagId → invalid
+      if (!tagId) {
+        return res.status(400).json({
+          status: "error",
+          message: "tag_id or new_tag_name/new_tag_color must be provided",
+        });
+      }
+
+      // Update session in db
+      const session = await prisma.session.create({
         data: {
           user_id: user.id,
-          name: body.new_tag_name,
-          color: body.new_tag_color,
-          created_at: new Date(),
+          name: body.name ? body.name : null,
+          start_at: startAt,
+          end_at: endAt,
+          break_time: 0,
+          status: SessionStatus.SCHEDULED,
+          tag_id: tagId,
+        },
+        include: {
+          tag: true,
         },
       });
 
-      tagId = newTag.id;
-    }
-
-    // If still no tagId → invalid
-    if (!tagId) {
-      return res.status(400).json({
-        status: "error",
-        message: "tag_id or new_tag_name/new_tag_color must be provided",
-      });
-    }
-
-    // Update session in db
-    const session = await prisma.session.create({
-      data: {
-        user_id: user.id,
-        name: body.name ? body.name : null,
-        start_at: startAt,
-        end_at: endAt,
-        break_time: 0,
-        status: SessionStatus.SCHEDULED,
-        tag_id: tagId,
-      },
-      include: {
-        tag: true,
-      },
-    });
-
-    return res.status(201).json({
-      status: "success",
-      data: {
-        id: session.id,
-        name: session.name,
-        start_at: session.start_at.toISOString(),
-        end_at: session.end_at ? session.end_at.toISOString() : null,
-        status: session.status,
-        break_time: session.break_time,
-        tag: {
-          id: session.tag.id,
-          name: session.tag.name,
-          color: session.tag.color,
+      return res.status(201).json({
+        status: "success",
+        data: {
+          id: session.id,
+          name: session.name,
+          start_at: session.start_at.toISOString(),
+          end_at: session.end_at ? session.end_at.toISOString() : null,
+          status: session.status,
+          break_time: session.break_time,
+          tag: {
+            id: session.tag.id,
+            name: session.tag.name,
+            color: session.tag.color,
+          },
         },
-      },
-    });
-  } catch (err) {
-    next(err);
+      });
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 //PATCH edit scheduled sessions only
 router.patch(
-  "/:id",
+  "/update/:id",
   async (req: Request, res: Response, next: NextFunction) => {
     //Extract user from request
     const { user } = req as AuthRequest;
@@ -375,7 +378,7 @@ router.patch(
 );
 
 router.delete(
-  "/:id",
+  "/delete/:id",
   async (req: Request, res: Response, next: NextFunction) => {
     // Extract user from req
     const { user } = req as AuthRequest;
@@ -466,7 +469,7 @@ router.get("/tags", async (req: Request, res: Response, next: NextFunction) => {
 });
 
 router.post(
-  "/tags",
+  "/add/tags",
   async (req: Request, res: Response, next: NextFunction) => {
     const { user } = req as AuthRequest;
 
