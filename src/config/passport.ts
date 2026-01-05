92846799750074102e41057075a1456ce9b6d2bb
HenryNgu1 <henryn.contact@gmail.com>
2026-01-05 14:57:04 +1100

    fix: await bug

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,4 +1,4 @@
-import passport, { use } from "passport";
+import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Prisma } from "@prisma/client";
 import prisma from "../libs/prisma.js";
